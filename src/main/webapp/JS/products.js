b3653b9b6b7d9047957b57829864cc1c0d3802ec
Rutger de Kok <de.kok.rutger@gmail.com>
2017-06-12 16:43:00 +0200

    Partial implementation of edit product. Edit link for the moment redirects to the rest/products/{id} page

--- src/main/webapp/JS/products.js
+++ src/main/webapp/JS/products.js
@@ -29,6 +29,7 @@ function startProductTable() {
     getCategories();
     $.getJSON('rest/products', function (data) {
         var datarow = " ";
+        var rowindex = 0;
         $.each(data, function (key, value) {
             datarow += '<tr>';
             datarow += ' <td align=/"left/">' + value.name + ' </td>';
@@ -38,10 +39,16 @@ function startProductTable() {
             datarow += ' <td align=/"left/">' + value.price + ' </td>';
             datarow += ' <td align=/"left/">' + value.stockCount + ' </td>';
             datarow += ' <td align=/"left/">' + value.info + ' </td>';
-            datarow += '</tr>';
+            datarow += ' <td align=/"left/" onclick=\"editRow(' + rowindex +','+ value.id+ ')\"> Edit </td> </tr>';
+            rowindex++;
         });
         $('#table_main').append(datarow);
     });
+}
+
+function editRow(index,id) {
+    alert("editRow = " +index+ " id = "+id);
+    window.location.href = "rest/products/"+id;
 }
 
 
