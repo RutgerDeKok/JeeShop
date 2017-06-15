889af5e463fc6af26172775c7f51517a51d92582
J-PC\J <jurjen.s@gmail.com>
2017-06-16 00:11:34 +0200

    accepteert jstring niet

--- src/main/webapp/JS/demo-products.js
+++ src/main/webapp/JS/demo-products.js
@@ -11,7 +11,7 @@ function startProductTable() {
             datarow += '<td id="price">' + value.price + ' </td>';
             datarow += '<td id="stockCount">' + value.stockCount + ' </td>';
             datarow += '<td id="info">' + value.info + ' </td>';
-            datarow += '<td id="editSave"><button id="edit" onclick=\"editRow(this)\">Edit</button></td></tr>';
+            datarow += '<td id="editSave"><button id="edit" onclick="editRow(this)">Edit</button></td></tr>';
         });
         datarow += '</tbody>';
         $('#productsTable').append(datarow);
@@ -38,10 +38,53 @@ function getCategory(cat) {
 function editRow(button) {
     button.parentNode.parentNode.className = 'highlight';
     button.parentNode.parentNode.setAttribute("contenteditable","true");
-    button.parentNode.innerHTML = '<button id="save">Save</button>';    
+    button.parentNode.innerHTML = '<button id="save" onclick="saveRow(this)">Save</button>';
 }
 
 function saveRow(button) {
+    button.parentNode.parentNode.className = '';
+    button.parentNode.parentNode.setAttribute("contenteditable","false");
+    var $row = jQuery(button).closest('tr');
+    var $columns = $row.find('td').not(':last');
+//    console.log($columns);
+    var values = "{";
+    var id = "";
+    jQuery.each($columns, function(i, item) {
+        //console.log($columns[i].id);
+        if ($columns[i].id === "id") {
+            id = item.innerHTML;
+            alert(id);
+        }
+        values += $columns[i].id + ":";
+        values = values.concat("\x22",item.innerHTML,"\x22",",");
+        //alert(values);
+    });
+    values = values.replace(/,$/,"}");
+    console.log(values);
+    console.log(JSON.stringify(values));
+    
+    $.ajax({
+                type: "PUT",
+                url: "rest/products/" + id,
+                data: values,
+                contentType: "application/json",
+                success: function () {
+                    alert("Succes!");
+                },
+                error: function () {
+                    alert("Error, "+ values);
+                }
+         }); 
+    
+}
+
+//function saveRow(button) {    
+//    button.parentNode.parentNode.className = '';
+//    button.parentNode.parentNode.setAttribute("contenteditable","false");
+//    var table = document.getElementById('productsTable'),
+//        highlight = table.getElementsByClassName('highlight');
+//    alert(button.parentNode.);
+    
 //    velden product:
 //            id
 //            brand
@@ -50,36 +93,23 @@ function saveRow(button) {
 //            name
 //            stockCount
     // convert row to json
-    // 
-    $("#save").click(function(){
-        var data = [];
-//        var el = document.getElementById("id");
-//        alert(el.getAttribute("id"));
-        $("#productsTable").find('tr.highlight').each(function(rowIndex, row) {
-            var cols = [];
-            $(this).find('td').not(':last').each(function(colIndex, col) {                      
-                cols.push($(this).text().trim());
-            });
-            data.push(cols);            
-        });
-        alert(data);
-        return data;
-    });
+    //
+//    $(".highlight").click(function () {
+//    var row = $(this).parent().parent().parent().html();
+//    alert(row);
+//    });
+//    $('.save').on('click',function() {
+//       var row = document.getElementById("id");
+//       alert(row);
+//       $(button).parent('table').find('td').each(function() {
+//           var colData = $(this).find('tr').eq(row).html();
+//           alert(colData);
+//       });
+//    });
     // 
     // send json to rest
     // 
-    //    $.ajax({
-    //            type: "PUT",
-    //            url: "rest/products/" + dataObject.id,
-    //            data: dataObject,
-    //            contentType: "application/json",
-    //            success: function () {
-    //                alert("Succes!");
-    //            },
-    //            error: function () {
-    //                alert("Error, "+ dataObject);
-    //            }
-    //     }); 
+        
     // remove highlight
     // show edit button instead of save again
-}
+//}
