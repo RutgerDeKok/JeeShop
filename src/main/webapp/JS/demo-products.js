acb41f7039417f35cc51eb5cbda291d4a156038c
Rutger de Kok <de.kok.rutger@gmail.com>
2017-06-18 22:33:49 +0200

    Improved the demo-page, pull down only poulated when edit row

--- src/main/webapp/JS/demo-products.js
+++ src/main/webapp/JS/demo-products.js
@@ -6,39 +6,40 @@ function startProductTable() {
             datarow += '<tbody><tr>';
             datarow += '<td id="id" hidden>' + value.id + '</td>';
             datarow += '<td id="name">' + value.name + ' </td>';
-            datarow += '<td id="category"><span id="catSpan'+index+'">'+ getCategory(value.category)+'</span>' ;
-
-            datarow += '<select id="catSel'+index+'" hidden>';
-            $.each(categories, function (i, cat) {
-                if (value.category === cat.name) {
-                    datarow += '<option value="' + cat.name + '" selected="selected">' + cat.readableName + '</option>';
-                } else {
-                    datarow += '<option value="' + cat.name + '">' + cat.readableName + '</option>';
-                }
-            });
-            datarow += '<option value = "' + value.category + '">' + getCategory(value.category) + '</option>';
-            datarow += '</select> </td>';
-            
+            datarow += '<td id="category"><span id="catSpan' + index + '">' + getCategory(value.category) + '</span>';
+            datarow += '<select id="catSel' + index + '" hidden></select> </td>';
             datarow += '<td id="brand">' + value.brand + ' </td>';
             datarow += '<td id="price">' + value.price + ' </td>';
             datarow += '<td id="stockCount">' + value.stockCount + ' </td>';
             datarow += '<td id="info">' + value.info + ' </td>';
-            datarow += '<td id="editSave"><button id="edit" onclick="editRow(this,'+index+')">Edit</button></td></tr>';
+            datarow += '<td id="editSave"><button id="edit" onclick="editRow(this,' + index + ')">Edit</button></td></tr>';
         });
         datarow += '</tbody>';
         $('#productsTable').append(datarow);
     });
 }
 
+
+
 function editRow(button, index) {
     button.parentNode.parentNode.className = 'highlight';
     button.parentNode.parentNode.setAttribute("contenteditable", "true");
-    button.parentNode.innerHTML = '<button id="save" onclick="saveRow(this,'+index+')">Save</button>';
+    button.parentNode.innerHTML = '<button id="save" onclick="saveRow(this,' + index + ')">Save</button>';
 //    button.parentNode.parentNode.find('.categorytext').hide();
-    var catSpan=$(document.getElementById('catSpan'+index));
-    var catSel=$(document.getElementById('catSel'+index));
-    catSel.show();
-    catSpan.hide();
+    var $catSpan = $(document.getElementById('catSpan' + index));
+    var $catSel = $(document.getElementById('catSel' + index));
+    
+    //opties toevoegen aan <select> menu
+    $catSel.html(' ');
+    $.each(categories, function (i, cat) {
+        if (getCategoryEnum($catSpan.text()) === cat.name) {
+            $catSel.append('<option value="' + cat.name + '" selected="selected">' + cat.readableName + '</option>');
+        } else {
+            $catSel.append('<option value="' + cat.name + '">' + cat.readableName + '</option>');
+        }
+    });
+    $catSel.show();
+    $catSpan.hide();
 
 }
 
@@ -71,40 +72,38 @@ function getCategoryEnum(readableName) {
 
 
 
-function saveRow(button,index) {
+function saveRow(button, index) {
     button.parentNode.parentNode.className = '';
 //    button.parentNode.parentNode.removeClass("highlight");
     button.parentNode.parentNode.setAttribute("contenteditable", "false");
     var $row = jQuery(button).closest('tr');
     var $columns = $row.find('td').not(':last');
-    
-    var catSpan=$(document.getElementById('catSpan'+index));
-    var catSel=$(document.getElementById('catSel'+index));
-    var catVal= catSel.val();
-    var catText = catSel.find('option:selected').text();
-  
-    catSpan.text(catText);
-    catSel.hide();
-    catSpan.show();
-//    console.log("columns: " + $columns);
+
+    var $catSpan = $(document.getElementById('catSpan' + index));
+    var $catSel = $(document.getElementById('catSel' + index));
+    var catVal = $catSel.val();
+    var catText = $catSel.find('option:selected').text();
+
+    $catSpan.text(catText);
+    $catSel.hide();
+    $catSpan.show();
+    $catSel.html(' ');
+    //    console.log("columns: " + $columns);
     var dataObject = {};
     jQuery.each($columns, function (i, item) {
 
-//        alert("name: " + $columns[i].id + ", value: " + item.innerHTML);
         if (item.value !== "") {
             if ($columns[i].id === "category") {
-//                var catprod = getCategoryEnum(item.innerHTML);
                 dataObject[$columns[i].id] = catVal;
             } else {
                 dataObject[$columns[i].id] = item.innerHTML;
             }
-//            alert("dataObject: " + dataObject);
         }
     });
 
     var id = dataObject.id;
     var jsonData = JSON.stringify(dataObject);
-//    alert("json: "+JSON.stringify(dataObject));
+    //    alert("json: "+JSON.stringify(dataObject));
     $.ajax({
         type: "PUT",
         url: "rest/products/" + id,
@@ -118,7 +117,7 @@ function saveRow(button,index) {
         }
     });
 
-    button.parentNode.innerHTML = '<button id="edit" onclick="editRow(this,'+index+')">Edit</button>';
+    button.parentNode.innerHTML = '<button id="edit" onclick="editRow(thi s ,' + index + ')">Edit</button>';
 // show edit button instead of save again
 }
 
