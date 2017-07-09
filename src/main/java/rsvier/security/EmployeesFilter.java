54588c644d13168bf2ef3a067c2af816e6447362
FRANK <lorus1@live.nl>
2017-07-09 15:44:50 +0200

    Toen ik nog niet nog inloggen zat dit er nog in, is nu weg.

--- src/main/java/rsvier/security/EmployeesFilter.java
+++ src/main/java/rsvier/security/EmployeesFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import rsvier.model.UserType;
 
-@WebFilter(filterName = "EmployeesFilter", urlPatterns = {"/employees6646554/*"})
+@WebFilter(filterName = "EmployeesFilter", urlPatterns = {"/employees/*"})
 public class EmployeesFilter implements Filter {
 
     @EJB
