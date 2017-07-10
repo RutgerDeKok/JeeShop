9bce7c7bf2d68e6a023d823e56197d1d9ca6e52a
FRANK <lorus1@live.nl>
2017-07-10 15:28:00 +0200

    v2.2 krijg een alert fout.

--- src/main/webapp/JS/edit-adresses.js
+++ src/main/webapp/JS/edit-adresses.js
@@ -20,10 +20,11 @@ function setupEditAdres() {
 //    prefill the input boxes with available product data
 
     $.getJSON('/Jee-Shop/rest/adresses/' + id, function (address) {
-        var address = user.billingAddress;
-        if (!address) {
-            address = {};
-        }
+//        NIET NODIG! ADres ziet al alles! User niet, dus die gebruikte een var om onnodig typen te voorkomen.
+//        var address = user.billingAddress; <------compleet onnodig hier.
+//        if (!address) {
+//            address = {};  <-- onzin die if.
+//        }
         $('#addressIdId').val(address.id);
         $('#voornaamId').val(address.firstName);
         $('#achternaamId').val(address.familyName);
@@ -73,41 +74,51 @@ function setupEditAdres() {
 
 function putPostAdresUser() {
 
+
+    // alles is gevuld, dus je hebt de data neem ik aan!
+
     var userObject = {};
     var addressObject = {};
 
     if ($('#addressIdId').val() > 0) {
         addressObject.id = $('#addressIdId').val();
     }
-    addressObject.firstName = $('#firstNameId').val();
+    else{
+     alert("dit gaat helemaal niet goed!")   ;
+    }
+    addressObject.firstName = $('#voornaamId').val();
     addressObject.insertion = $('#insertionId').val();
-    addressObject.familyName = $('#familyNameId').val();
-    addressObject.street = $('#streetId').val();
-    addressObject.number = $('#numberId').val();
+    addressObject.familyName = $('#achternaamId').val();
+    addressObject.street = $('#straatId').val();
+    addressObject.number = $('#nummerId').val();
     addressObject.numAddition = $('#additionId').val();
     addressObject.zipCode = $('#zipCodeId').val();
-    addressObject.city = $('#cityId').val();
+    addressObject.city = $('#stadId').val();
 
-    userObject.id = $('#IdId').val();
-    alert("hash = " + $('#hashId').val());
-    userObject.passHash = $('#hashId').val();
-    userObject.email = $('#emailId').val();
-    userObject.type = $('#typeId').val();
-    userObject.billingAddress = addressObject;
+//NU heb ik hopelijk een addressObject die naar de Database Kan!
 
 
-    var jsonData = JSON.stringify(userObject);
+//twijfel of dit nodig is.
+//    userObject.id = $('#IdId').val();
+//    alert("hash = " + $('#hashId').val());
+//    userObject.passHash = $('#hashId').val();
+//    userObject.email = $('#emailId').val();
+//    userObject.type = $('#typeId').val();
+//    userObject.billingAddress = addressObject; <-- sneaky zin
+
+
+    var jsonData = JSON.stringify(addressObject);
     console.log(jsonData);
     alert(jsonData);
 
-    var id = userObject.id;
-//    alert("id= " + id);
+    var id = addressObject.id;
+    alert("id= " + id);
 
     if (id !== undefined) {
-//        alert("PUT");
+        alert("PUT");
         $.ajax({
             type: "PUT",
-            url: "/Jee-Shop/rest/users/" + id,
+            url: "/Jee-Shop/rest/addresses/" + id,
             data: jsonData,
 //            dataType: "json", alleen nodig als return data wordt 
             contentType: "application/json",
@@ -116,6 +127,7 @@ function putPostAdresUser() {
                 setTimeout(function () {
                     window.close();
                 }, 200);
+ //               Dit was een controle zin met de users om te kijken of het werkte
 //                window.location.href = "/Jee-Shop/employees/users.html";
             },
             error: function () {
@@ -132,7 +144,7 @@ function putPostAdresUser() {
             contentType: "application/json",
             success: function () {
 
-                window.location.href = "/Jee-Shop/employees/users.html";
+                window.location.href = "/Jee-Shop/customer_confirmation.html";
             },
             error: function () {
                 alert("Error, " + jsonData);
